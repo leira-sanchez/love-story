2ef5ad3a3dc58d324da6fc98d56bd5a977e2c6c7
Leira C Sanchez Quinones <leira.sq@gmail.com>
2021-07-29 18:38:31 -0400

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Header from "./Header";
-import Horoscopo from "./Horoscopo";
-import InstaGallery from "./InstaGallery";
-import Modal from "./Modal";
-import Nosotras from "./Nosotras";
-import TinderCard from "./TinderCard";
-
-export { Header, Horoscopo, InstaGallery, Modal, Nosotras, TinderCard };
